819f7feaa632bffbfff25d13f7d11e7d8e91cc4a
Denis <denkazarin@icloud.com>
2021-06-03 00:47:19 +0200

    Home work 18 fixes
    fix

--- Denis_Kazarin/Homework-18/home_work18.js
+++ Denis_Kazarin/Homework-18/home_work18.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Task 1
 /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}(\-|\.)?[a-z\d]{1,10}.com$/i.test('abcd_aaa-7778@gmail.com');
 
@@ -16,26 +15,6 @@ function vowelQuantity(string) {
     console.log('Гласных не найдено');
   } else {
       return check.length;
-=======
-  /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}(\-|\.)?[a-z\d]{1,10}.com$/i.test('abcd_aaa-7778@gmail.com');
-
-
-    function numberCheck(number) {
-        return /^\+?([375]{3}(\-)?|[8]{1}(\-)?[0]{1})([25]{2}(\-)?|[29]{2}(\-)?|[33]{2}(\-)?|[44]{2}(\-)?|[17]{2}(\-)?)[1-9]{1}[\d]{2}(\-)?[\d]{2}(\-)?[\d]{2}(\-)?$/i.test(number);
-      }
-      numberCheck('8-029-555-55-54');
-
-
-
-    function vowelQuantity(string) {
-        var check = string.match(/[уеыаоэёяию]/ig);
-
-        if (!check) {
-          console.log('Гласных не найдено');
-        } else {
-          return check.length;
-        }
->>>>>>> D.Kazarin_Homework-18
     }
 }
 vowelQuantity('кофемашина');
