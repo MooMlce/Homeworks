1d7d18cfb84a8392a674d405daecb828035348d2
Daniil <danila.glushakov@mail.ru>
2021-06-03 02:00:23 +0300

    Homework 18 fixes
    Fix defect

--- Daniil_Glushakov/Homework-18/1.js
+++ Daniil_Glushakov/Homework-18/1.js
@@ -1,32 +1,21 @@
-/^([a-z]{3,10}[_]{1}[a-z]{3,10})([-0-9]{5})?@{1}([a-z]{1,10}([.-]{1})?[a-z]{1,10}).com$/i.test('name_surname-1234@gmail.com');
-
-
-
-
+/^([a-z]{3,10}_[a-z]{3,10})-?([0-9]{4})?@([a-z]{1,10}([.-])?[a-z]{1,10}).com$/i.test(
+  'name_surname-1234@gmail.com'
+);
 
 function validate(strings) {
   var pattern =
-    /^(\+?375\-?(25|29|33|44|17)\-?[1-9]{1}\d{2}\-?\d{2}\-?\d{2})|(8\-?0(25|29|33|44|17)\-?[1-9]{1}\d{2}\-?\d{2}\-?\d{2})$/;
+    /^((\+?375\-?)|(8\-?0))(25|29|33|44|17)\-?[1-9]{1}\d{2}(-?\d{2}){2}$/;
   var regex = new RegExp(pattern, 'i');
   return regex.test(strings);
 }
 validate('+375-25-777-77-77');
 
-
-
-
-
 function getVowels(str) {
   var vowels = str.match(/[aeiouёуеыаоэяию]/gi);
-  return vowels.length;
+  if (vowels == null) {
+    return 0;
+  } else {
+    return vowels.length;
+  }
 }
 getVowels('Привет друг, сколько тут гласных?');
-
-
-
-function findVowels(strs) {
-  var pattern = /[aeiouёуеыаоэяию]/;
-  var re = new RegExp(pattern, 'ig');
-  return strs.match(re).length;
-}
-findVowels('Привет друг, сколько тут гласных?');
