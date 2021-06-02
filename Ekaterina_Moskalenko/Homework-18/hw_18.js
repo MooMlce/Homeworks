ca2ba16fd6b43bfb2069c15c74a5178c17f1bce0
KatyaMoskalenko <tukai_ekaterina@mail.ru>
2021-06-02 21:46:48 +0300

    Homework 18 fixes
    fix code

--- Ekaterina_Moskalenko/Homework-18/hw_18.js
+++ Ekaterina_Moskalenko/Homework-18/hw_18.js
@@ -1,20 +1,21 @@
-/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@([a-z\d]+[-\.])?[a-z\d]{2,20}\.com$/i.test('name_surname-1234@gmail.com');
+/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}[.-]?[a-z\d]{1,10}\.com$/i.test('name_surname-1234@gmai.l.com');
 
 
-function validate(pattern){
-  var regExp = /^(8|(\+?375))[\-]?[0]?(29|25|44|33|17)[\-]?[^0]\d{2}[\-]?\d{2}[\-]?\d{2}$/;
-  return regExp.test(pattern);
+function validate(pattern) {
+	var regExp = /^(8-?0|\+?375-?)(29|25|44|33|17)-?[1-9]\d{2}(-?\d{2}){2}$/;
+	
+	return regExp.test(pattern);
 }
 
-validate('375299999999');
+validate('375-0299999999');
 
 
-function countVowels(str){
-  var reg = /[eyuioaаеиоуюяыёэ]/igm;
-  	if(str.match(reg) == null){
-    	console.log(0); 
+function countVowels(str) {
+	var reg = /[eyuioaаеиоуюяыёэ]/igm;
+	if(str.match(reg) == null) {            
+		console.log(0); 
   	} else {
-   		console.log(str.match(reg).length);
+   	 	console.log(str.match(reg).length);
  	}
 }
 
