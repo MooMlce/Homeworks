5f5b391299403621271b0641c8ea7bd6e2346a36
Natallya <natasupernice@gmail.com>
2021-06-02 23:44:29 +0300

    Homework 18
    Fix emails and phone regex

--- Natallya_Kantseliarchyk/Homevork-18/Lesson18.js
+++ Natallya_Kantseliarchyk/Homevork-18/Lesson18.js
@@ -1,15 +1,15 @@
 
-/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}[-.]?[a-z\d]{1,10}\.com$/i.test
-/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@(?:[a-z\d]{1,10}-?)[-.]?[a-z\d]{1,10}\.com$/i.test
+/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}[-.]?[a-z\d]{1,10}\.com$/i.test('name_surname-1234@gmail.com');
+/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@(?:[a-z\d]{2,20}-?)[-.]?[a-z\d]+\.com$/i.test('name_surname-1234@gmail.com');
 
 // Написать функцию, которая с помощью регулярного выражения будет тестировать на соответствие строки вида:
 function testNumber(number) {
-  return /^(\+?375-?|8-?0)(25|29|33|44|17)-?[1-9]{3}(-?[1-9]{2}){2}$/.test(number)
+  return /^(\+?375-?|8-?0)(25|29|33|44|17)-?[1-9]\d{2}(-?\d{2}){2}$/.test(number);
 }
 testNumber('375299999999');
 
 // Переписать решение задачи с поиском гласных
-  function textString(str) {
+function textString(str) {
   var count = 0;
 
   str.toLowerCase().split('').forEach(function(letter) {
