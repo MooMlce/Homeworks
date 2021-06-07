8fbe7eca9c27b05da31eb6175a780bec6a900d8c
Anna <anna.vl.gromova@gmail.com>
2021-06-07 11:54:54 +0300

    Homework 19
    Add homework

---
+++ Anna_Gromova/Homework-19/main.js
@@ -0,0 +1,114 @@
+var stopWatch = document.getElementsByClassName('stop-watch')[0],
+    startButton = document.getElementsByClassName('start-button')[0],
+    marks = document.getElementsByClassName('marks')[0],
+    minutes = document.querySelector('.minutes'),
+    seconds = document.querySelector('.seconds'),
+    milliseconds = document.querySelector('.milliseconds'),
+    state = stopWatch.dataset.state,
+    timerId,
+    resIndex = 1;
+    
+var ms = 0,
+    sec = 0,
+    min = 0;
+var timerId = null;
+
+
+
+startButton.addEventListener('click', startRunning);
+
+function changeEventListener(){
+    if (stopWatch.dataset.state == "idle" || stopWatch.dataset.state == "stopped") {
+      startButton.innerText = 'Stop';
+      stopWatch.dataset.state = "running";
+      startButton.removeEventListener('click', startRunning);
+      startButton.addEventListener('click', stopRunning);
+    }
+    else  {
+      startButton.innerText = 'Run';
+      stopWatch.dataset.state = "stopped";
+      startButton.removeEventListener('click', stopRunning);
+      startButton.addEventListener('click', startRunning);
+    } 
+}
+
+function startRunning() {
+    timerId = setInterval(tick, 10);
+    changeEventListener();
+    createMarkButtons();
+}
+
+function tick(timerId) {
+    ms ++;
+    if (ms === 100) {
+        ms = 0;
+        sec ++;
+        if (sec === 60) {
+            sec = 0;
+            min ++;  
+        } 
+    }
+    milliseconds.innerHTML = (ms < 10) ? "0" + ms : ms;
+    minutes.innerHTML = (min < 10) ? "0" + min : min;
+    seconds.innerHTML = (sec < 10) ? "0" + sec : sec;
+    
+    if (min === 60 && sec === 59 && ms === 99) {
+        timerId = null;
+		stopRunning();
+	}
+}
+
+function stopRunning() {
+    changeEventListener();
+    clearInterval(timerId);
+}
+
+function createMarkButtons() {
+    marks.innerHTML = '';
+    var resetButton = document.createElement('button'),
+        saveButton = document.createElement('button');
+
+        resetButton.innerText = 'Reset';
+        saveButton.innerText ='Save';
+        saveButton.classList.add('save');
+
+    resetButton.addEventListener('click', resetStopWatch);
+    saveButton.addEventListener('click', saveResult);
+    
+    marks.appendChild(resetButton);
+    marks.appendChild(saveButton);
+}
+
+function resetStopWatch() {
+    clearInterval(timerId);
+    stopWatch.dataset.state = "idle";
+    startButton.innerText = 'Start';
+    milliseconds.innerHTML = '00';
+	minutes.innerHTML = '00';
+	seconds.innerHTML = '00';
+    document.querySelector('.save').remove();
+
+    startButton.addEventListener('click', startRunning);
+    localStorage.clear();
+}
+
+function saveResult() {
+    var result = document.createElement('div');
+        result.innerHTML = resIndex++ + ') ' + minutes.innerHTML + ' : '  + seconds.innerHTML + ' : ' + milliseconds.innerHTML;
+
+    marks.appendChild(result);
+}
+
+
+// Написать свой секундомер в формате mm:ss:msms (по 2 цифры в каждом параметре).
+//   +  Изначально на странице должна быть кнопка "Start". При запуске секундомера текст кнопки меняется на "Stop".
+//    + Если пользователь нажимает на кнопку "Stop" - ее текст должен измениться на "Run".
+//   +  Использовать data-атрибут, хранящий состояние секундомера. Работа кнопки и секундомера должна опираться на него.
+
+//   +  Также после старта работы секундомера должны появиться кнопки "Save" и "Reset".
+//    + Кнопки должны работать соответственным образом (по клику на кнопку "Reset" секундомер должен полностью вернуться в изначальное состояние).
+//   -  Максимальное количество минут - 60. После этого секундомер останавливается (тестировать на значениях поменьше).
+//    - Должны остаться только кнопка "Reset" и метки.
+//     * Секундомер должен работать после перезагрузки страницы и полностью сохранять свое состояние и метки.
+//     Чтобы время шло со скоростью реального - запускать интервал с промежутком в 10 ms, увеличивать значение ms на 1 на каждой итерации и считать их до 100.
+//     При реализации класс Date использовать запрещено.
