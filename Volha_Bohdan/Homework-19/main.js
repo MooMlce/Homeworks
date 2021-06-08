5e1c2a5776a457f756803cf4154b0277ed388a51
Volha <volha.bohdan@gmail.com>
2021-06-08 11:11:10 +0300

    правки по дз 19

--- Volha_Bohdan/Homework-19/main.js
+++ Volha_Bohdan/Homework-19/main.js
@@ -1,8 +1,14 @@
-var timeContainer = document.getElementById('stopwatch');
-var startButton = document.getElementById('start');
-var resetButton = document.getElementById('reset');
-var saveButton = document.getElementById('save');
-var savedTimeContainer = document.getElementById('saved-time');
+var timeContainer = document.getElementById("stopwatch");
+var startButton = document.getElementById("start");
+startButton.addEventListener('click', startOrStop, false);
+
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener('click', reset, false);
+
+var saveButton = document.getElementById("save");
+saveButton.addEventListener('click', save, false);
+
+var savedTimeContainer = document.getElementById("saved-time");
 
 var ms = 0,
   sec = 0,
@@ -11,11 +17,11 @@ var ms = 0,
   savedTime = [];
 
 document.addEventListener("DOMContentLoaded", function () {
-  var stateLocal = localStorage.getItem('state');
+  var stateLocal = localStorage.getItem("state");
   if (stateLocal !== null) {
     var state = JSON.parse(stateLocal);
     timeContainer.dataset.enabled = state.state;
-    if (state.state === 'started') {
+    if (state.state === "started") {
       timerId = setInterval(timer, 10);
     }
 
@@ -26,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (state.savedTime.length > 0) {
       state.savedTime.forEach(function (time) {
-        var el = document.createElement('LI');
+        var el = document.createElement("LI");
         savedTime.push(time);
         el.innerHTML = time;
         savedTimeContainer.appendChild(el);
@@ -48,8 +54,7 @@ function timer() {
     sec = 0;
   }
   if (min === 60) {
-    ms, sec, min = 0;
-    timeContainer.dataset.enabled = 'finished';
+    timeContainer.dataset.enabled = "finished";
     clearInterval(timerId);
     updateButtonState();
   }
@@ -58,13 +63,13 @@ function timer() {
 };
 
 function startOrStop() {
-  if (timeContainer.dataset.enabled === 'init' || timeContainer.dataset.enabled === 'paused') {
+  if (timeContainer.dataset.enabled === "init" || timeContainer.dataset.enabled === "paused") {
     timerId = setInterval(timer, 10);
-    timeContainer.dataset.enabled = 'started';
+    timeContainer.dataset.enabled = "started";
     updateButtonState();
   } else {
     clearInterval(timerId);
-    timeContainer.dataset.enabled = 'paused';
+    timeContainer.dataset.enabled = "paused";
     updateButtonState();
   }
 }
@@ -74,13 +79,14 @@ function reset() {
   sec = 0;
   min = 0;
   clearInterval(timerId);
-  timeContainer.dataset.enabled = 'init';
+  timeContainer.dataset.enabled = "init";
   updateButtonState();
   localStorage.clear();
+  savedTime = [];
 }
 
 function save() {
-  var el = document.createElement('LI');
+  var el = document.createElement("LI");
   var timeToSave = formatTimeOutput(min, sec, ms);
   el.innerHTML = timeToSave;
   savedTime.push(timeToSave);
@@ -88,47 +94,47 @@ function save() {
 }
 
 function formatTimeOutput(min, sec, ms) {
-  var milli = ms < 10 ? '0' + ms : ms;
-  var seconds = sec < 10 ? '0' + sec : sec;
-  var minute = min < 10 ? '0' + min : min;
+  var milli = ms < 10 ? "0" + ms : ms;
+  var seconds = sec < 10 ? "0" + sec : sec;
+  var minute = min < 10 ? "0" + min : min;
 
-  return minute + ':' + seconds + ':' + milli;
+  return minute + ":" + seconds + ":" + milli;
 }
 
 function updateButtonState() {
   var currentState = timeContainer.dataset.enabled;
   switch (currentState) {
-    case 'init':
-      timeContainer.innerHTML = '00:00:00';
+    case "init":
+      timeContainer.innerHTML = "00:00:00";
       savedTimeContainer.innerHTML = '';
-      startButton.innerHTML = 'Start';
+      startButton.innerHTML = "Start";
       resetButton.hidden = true;
       saveButton.hidden = true;
       startButton.hidden = false;
       break;
-    case 'started':
-      startButton.innerHTML = 'Stop';
+    case "started":
+      startButton.innerHTML = "Stop";
       resetButton.hidden = false;
       saveButton.hidden = false;
       break;
-    case 'paused':
-      startButton.innerHTML = 'Run';
+    case "paused":
+      startButton.innerHTML = "Run";
       break;
-    case 'finished':
+    case "finished":
       saveButton.hidden = true;
       startButton.hidden = true;
   }
 }
 
 window.onbeforeunload = function () {
-  if (timeContainer.dataset.enabled !== 'init') {
+  if (timeContainer.dataset.enabled !== "init") {
     var objectToSave = {
-      'state': timeContainer.dataset.enabled,
-      'minutes': min,
-      'seconds': sec,
-      'miliseconds': ms,
-      'savedTime': savedTime
+      "state": timeContainer.dataset.enabled,
+      "minutes": min,
+      "seconds": sec,
+      "miliseconds": ms,
+      "savedTime": savedTime
     };
-    localStorage.setItem('state', JSON.stringify(objectToSave));
+    localStorage.setItem("state", JSON.stringify(objectToSave));
   }
 };
