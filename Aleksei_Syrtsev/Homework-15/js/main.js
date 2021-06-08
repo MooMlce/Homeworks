97485973a715d8d33f332f01de1593a7c3e129f3
AlexSyrtsev <aleksei.turchin@gmail.com>
2021-06-08 08:31:00 +0300

    Homework 15 fixes
    Fix stopwatch

--- Aleksei_Syrtsev/Homework-15/js/main.js
+++ Aleksei_Syrtsev/Homework-15/js/main.js
@@ -17,7 +17,7 @@ var min = document.getElementById('minutes'),
     msms = 0;
 
 function letsStart(){
-  clock.dataset.status = 'running';
+	clock.dataset.status = 'running';
 	push.innerText = 'PAUSE';
 
 	if(!document.getElementById('push-to-refresh')) {
@@ -26,19 +26,20 @@ function letsStart(){
 	var arr = setInterval(function() {
 		msms = msms + 1;
 		mil.innerText = (msms > 9) ? msms : '0' + msms;
-    		if (msms == 99) {
-          msms = 0;
-    			ss = ss + 1;
-    			mil.innerText = msms;
-    			sec.innerText = (ss > 9) ? ss : '0' + ss;	
-    		} else if (ss == 59){
-          ss = 0;
-    			mm = mm + 1;
-    			sec.innerText = ss;
-   				min.innerText = (mm > 9) ? mm : '0' + mm;		
-    		} else if (mm == 00 && ss == 05 && msms == 99) {
-    			clearCounter();	
-    	}
+		if (msms == 100) { // было 99 и "проскок" потолка;
+			msms = 0;
+			ss = ss + 1;
+			mil.innerText = msms;
+			sec.innerText = (ss > 9) ? ss : '0' + ss;	
+		} else if (ss == 60){ // было 59 и пропуск секунды;
+			ss = 0;
+			mm = mm + 1;
+			sec.innerText = '0' + ss; // // было так sec.innerText = ss; после 59 сек. шёл один, а не два нуля;
+			min.innerText = (mm > 9) ? mm : '0' + mm;		
+		// } else if (mm == 59 && ss == 59 && msms == 99) { // часовой "потолок" секундомера;
+		} else if (mm == 00 && ss == 03 && msms == 99) { // тестовый низкий "потолок" секундомера;
+			clearCounter();	
+		}
 
 	}, 10);
 
@@ -55,8 +56,8 @@ function letsPause(){
 }
 
 function createElements(){
-	clock.insertAdjacentHTML('afterEnd', '<div class="new-line"><input type="button" id="push-to-refresh" class="push-to-refresh" value="RESET">' + '<input type="button" id="results" class="results" value="SAVE"><div class="saved-ones" id="saved-ones"></div></div>');
-	document.getElementsByClassName('results')[0].addEventListener('click', keepResults);
+	clock.insertAdjacentHTML('afterEnd', '<div class="new-line"><input type="button" id="push-to-refresh" class="push-to-refresh" value="RESET">' + '<input type="button" id="allresults" class="allresults" value="SAVE"><div class="saved-ones" id="saved-ones"></div></div>');
+	document.getElementsByClassName('allresults')[0].addEventListener('click', keepResults);
 	document.getElementsByClassName('push-to-refresh')[0].addEventListener('click', letsRefresh);
 }
 
@@ -68,28 +69,28 @@ function keepResults(){
 
 function letsRefresh(){
 	mm = 0;
-  ss = 0;
-  msms = 0;
+	ss = 0;
+	msms = 0;
 
-  clock.dataset.status = 'initial';
+	clock.dataset.status = 'initial';
 	push.innerText = 'START';
 	clearInterval(countIt);
-  
-  min.innerText = '0' + mm;
-  sec.innerText = '0' + ss;
-	mil.innerText = '0' + msms;
 	
-	document.getElementsByClassName('results')[0].remove();
+	min.innerText = '0' + mm;
+	sec.innerText = '0' + ss;
+	mil.innerText = '0' + msms;
+		
+	if (document.getElementsByClassName('allresults').length) {
+		document.getElementsByClassName('allresults')[0].remove();
+	}
 	document.getElementsByClassName('push-to-refresh')[0].remove();
-  document.getElementsByClassName('saved-ones')[0].remove();
+	document.getElementsByClassName('saved-ones')[0].remove();
 }
 
 function clearCounter () {
-  clearInterval(countIt);
+	clearInterval(countIt);
 
-  document.getElementsByClassName('results')[0].remove();
-	document.getElementsByClassName('push-to-refresh')[0].remove();
-  document.getElementsByClassName('saved-ones')[0].remove();
+	document.getElementsByClassName('allresults')[0].remove();
 };
 
 clock.style.display = "flex";
