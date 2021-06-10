25154cc27b377fe18b4ea9250f82305d1e0dd26d
Pavel <pleshkov.p@gmail.com>
2021-06-10 22:44:07 +0300

    Homework 22
    refactor: beautify code

--- Pavel_Pleshkov/Homework-22/main.js
+++ Pavel_Pleshkov/Homework-22/main.js
@@ -3,8 +3,8 @@
 function countVowelLetters(text) {
     const vowelLetters = ['а', 'я', 'ы', 'и', 'о', 'ё', 'у', 'ю', 'э', 'е', 'a', 'e', 'i', 'o', 'u', 'y'];
 
-    return [...text.toLowerCase()].filter( letter => {
-         return vowelLetters.includes(letter);
+    return [...text.toLowerCase()].filter(letter => {
+        return vowelLetters.includes(letter);
     }).length;
 }
 
